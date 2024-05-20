b60cd30159e2e5d491c5ec1807210fbc92c6f61a
Emil Piękoś <piekosemil@gmail.com>
2024-05-20 15:35:03 +0200

    Poprawiony kod.
    Obecnie metoda coolDown obniża temperaturę tylko do określonego poziomu.
    Jednak metoda coolDownIsPossible, która ma zwracać true jeśli udało się obniżyc temperaturę nie zwraca wartości true jeśli nie osiągnie temperatury minimalnej.
    
    Próbowałem różnych możliwości, żeby wyświetlany rezultat zgadzał się z logiką działania klimatyzacji. Czyli jeśli udało się obniżyć temperaturę z 23.7 do 19.7 to powinna wyświetlić się wartość true jednak. Jednak nie mam pojęcia jak uzależnić aktualną temperaturę od temperatury minimalnej. Aktualnie metoda coolDownIsPossible zwraca true tylko wtedy kiedy osiągnięta jest temperatura minimalna. Nie wiem jak uzależnić wyświetlanie informacji o obniżeniu temperatury od różnicy temperatury początkowej i końcowej.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,18 +3,20 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Room room1 = new Room(15, 23, 19, true);
+        Room room1 = new Room(15, 23.7, 19.5, true);
         Room room2 = new Room(17, 23, 21, false);
         Room room3 = new Room(12, 17, 19, true);
+        Room room4 = new Room(10, 20, 19, true);
 
         System.out.println("Temperatura w pokoju nr 1: " + room1.temperature);
-        room1.coolDown(); // 22
-        room1.coolDown(); // 21
-        room1.coolDown(); // 20
-        room1.coolDown(); // 19
-        room1.coolDown(); // 18 - to już się nie wykonuje
-        room1.coolDown(); // 17 - to już się nie wykonuje
+        room1.coolDown();
+        //room1.coolDown();
+        room1.coolDown();
+        System.out.println("Obniżona temperatura: " + room1.temperature);
+        room1.coolDown();
+        room1.coolDown();
         System.out.println("Temperatura w pokoju nr 1: " + room1.temperature);
+        System.out.println("Czy udało się obniżyć temperaturę? : " + room1.coolDownIsPossible());
         System.out.println();
 
         System.out.println("Temperatura w pokoju nr 2: " + room2.temperature);
@@ -22,7 +24,9 @@ public class Main {
         room2.coolDown();
         room2.coolDown();
         room2.coolDown();
+        room2.coolDown();
         System.out.println("Temperatura w pokoju nr 2: " + room2.temperature);
+        System.out.println("Czy udało się obniżyć temperaturę? : " + room2.coolDownIsPossible());
         System.out.println();
 
         System.out.println("Temperatura w pokoju nr 3: " + room3.temperature);
@@ -31,8 +35,13 @@ public class Main {
         room3.coolDown();
         room3.coolDown();
         room3.coolDown();
-        room3.coolDown();
         System.out.println("Temperatura w pokoju nr 3: " + room3.temperature);
+        System.out.println("Czy udało się obniżyć temperaturę? : " + room3.coolDownIsPossible());
+        System.out.println();
+
+        System.out.println("Temperatura w pokoju nr 4: " + room4.temperature);
+        System.out.println("Temperatura w pokoju nr 3: " + room4.temperature);
+        System.out.println("Czy udało się obniżyć temperaturę? : " + room4.coolDownIsPossible());
         System.out.println();
     }
 }
--- src/main/java/pl/javastart/task/Room.java
+++ src/main/java/pl/javastart/task/Room.java
@@ -12,10 +12,34 @@ public class Room {
         this.minTemperature = minTemperature;
         this.acMounted = acMounted;
     }
+/* Metoda powinna zwracać true, jeżeli udało się obniżyć temperaturę,
+lub false jeżeli w pokoju nie ma klimatyzatora
+lub osiągnięto temperaturę graniczną.
+ */
 
     void coolDown() {
-        if (acMounted && (temperature > minTemperature)) {
-            temperature--;
+        if (acMounted) {
+            if (temperature > minTemperature) {
+                if (temperature - 1 > minTemperature) {
+                    temperature--;
+                } else {
+                    temperature = temperature - (temperature - minTemperature);
+                }
+            }
         }
     }
+
+    boolean coolDownIsPossible() {
+        if (!(acMounted && temperature > minTemperature || temperature != minTemperature)) {
+            return true;
+        } else if (acMounted && temperature > minTemperature) {
+            if (temperature < minTemperature) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
+//return acMounted && temperature < minTemperature;
